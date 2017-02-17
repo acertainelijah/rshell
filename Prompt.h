693f44a97bd32f7e5badcd1d40d1b7f83be865a8
Dakota Whelchel <dwhel001@ucr.edu>
2017-02-17 00:00:14 -0800

    Finished the prompt class.

--- Prompt.h
+++ Prompt.h
@@ -1,14 +1,56 @@
 #ifndef _PROMPT_H_
 #define _PROMPT_H_
+#include <unistd.h>
+#include <cstdlib>
+#include <sstream>
+#include <stdio.h>
+#include <vector>
 #include "CommandPrompt.h"
 
-class Prompt : public CommandPrompt { //Leaf that simply displays the prompt 
+class Prompt : public CommandPrompt { //Leaf that simply displays the prompt
 
-  public:
-    Prompt(){};
-    void execute() {
-		cout << "$ ";		//simply displays prompt
+private:
+  string userInput;
+  vector<string> parsedUserInput;
+
+public:
+  Prompt(){};
+  void execute() {
+    char *name;                                         //declare the name variable
+    name = (char *)malloc(100*sizeof(char));
+    getlogin_r(name, 100);                              //get user name
+    char *hostname;                                     //declare hostname variable
+    hostname = (char *)malloc(1000*sizeof(char));
+    gethostname(hostname, 1000);                        //gets the host name
+
+    while(true){
+
+      parsedUserInput.clear();
+
+      cout << name << '@' << hostname << "$ ";		      //simply displays prompt
+
+      getline(cin, userInput);
+      stringstream ss(userInput);
+
+      while(ss){
+        string sub;
+        ss >> sub;
+
+        parsedUserInput.push_back(sub);
+      }
+
+      parsedUserInput.pop_back();
+
+      // for(unsigned i = 0; i < parsedUserInput.size(); ++i){
+      //   cout << parsedUserInput.at(i) << endl;
+      // }
+
+      CommandPrompt* object = new Command(parsedUserInput);
+      object->execute();
+
     }
+
+  }
 
 };
 
